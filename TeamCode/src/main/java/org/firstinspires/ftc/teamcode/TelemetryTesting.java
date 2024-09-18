3470750f99c464907bdf71be2f0b388709456a01
austindeng <austindeng3@gmail.com>
2024-09-18 14:21:57 -0500

    Create TelemetryTesting.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTesting.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="TelemetryTesting", group = "test")
+public class TelemetryTesting extends OpMode {
+
+    DcMotor fr, bl;
+    @Override
+    public void init() {
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("horizontal", fr.getCurrentPosition());
+        telemetry.addData("vertical", bl.getCurrentPosition());
+    }
+}
