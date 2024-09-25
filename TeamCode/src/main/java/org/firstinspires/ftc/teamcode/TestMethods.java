ab08788e525571da12b5b0c7ce2f220a44c898a5
austindeng <austindeng3@gmail.com>
2024-09-25 15:17:17 -0500

    Update TestMethods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMethods.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Line;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,9 +13,31 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TestMethods{
     DcMotor fl, bl, fr, br;
 
+    BNO055IMU imu;
+
     OpMode opMode;
     public TestMethods(LinearOpMode opMode){
         this.opMode = opMode;
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample OpMode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
+
         fl = opMode.hardwareMap.get(DcMotorEx.class, "fl");
         fr = opMode.hardwareMap.get(DcMotorEx.class, "fr");
         br = opMode.hardwareMap.get(DcMotorEx.class, "br");
@@ -30,6 +54,8 @@ public class TestMethods{
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
 
 
     }
