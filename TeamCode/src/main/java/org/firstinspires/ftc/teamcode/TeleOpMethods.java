2bcbeab8fbb86c04cac0454e59705a3cc7744e2c
Katya K. <117482321+nkalaichidi79@users.noreply.github.com>
2024-10-07 16:26:47 -0500

    Software Meeting (10/7)
    TeleOp
    Game Pad Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMethods.java
@@ -15,10 +15,48 @@ public class TeleOpMethods {
     {
         drivetrain(gamepad1, gamepad2);
     }
-    public void drivetrain(Gamepad gamepad1, Gamepad gamepad2)
-    {
-        robot.drivetrain.fl.setPower(1);
+    public void drivetrain(Gamepad gamepad1, Gamepad gamepad2) {
+        double FLP, FRP, BRP, BLP;
+        FLP = 0;
+        FRP = 0;
+        BRP = 0;
+        BLP = 0;
+
+        // Moves forward and back
+        if (Math.abs(gamepad1.left_stick_y) > 0.1){
+            FLP += gamepad1.left_stick_y;
+            FRP += gamepad1.left_stick_y;
+            BLP += gamepad1.left_stick_y;
+            BRP += gamepad1.left_stick_y;
+        }
+
+
+        //Moves right and left
+        if (Math.abs(gamepad1.right_stick_x) > 0.1) {
+            FLP -= gamepad1.right_stick_x;
+            FRP += gamepad1.right_stick_x;
+            BLP -= gamepad1.right_stick_x;
+            BRP += gamepad1.right_stick_x;
+        }
+
+
+
+        // Strafe right and left
+        if (Math.abs(gamepad1.left_stick_x) > 0.1) {
+            FLP -= gamepad1.left_stick_x;
+            FRP += gamepad1.left_stick_x;
+            BLP += gamepad1.left_stick_x;
+            BRP -= gamepad1.left_stick_x;
+        }
+
+        robot.drivetrain.fl.setPower(FLP);
+        robot.drivetrain.fr.setPower(FRP);
+        robot.drivetrain.bl.setPower(BLP);
+        robot.drivetrain.br.setPower(BRP);
+
+
     }
+
 
 
 
