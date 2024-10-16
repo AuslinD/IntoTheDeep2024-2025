85ab2f978ca37f3139c0a25fb87c21993446061b
austindeng <austindeng3@gmail.com>
2024-10-16 15:47:02 -0500

    automethods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMethods.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AutoMethods {
+
+    OpMode opMode;
+    public AutoMethods(OpMode opMode){
+        this.opMode = opMode;
+    }
+
+
+    public void encoderDrive(double power, int ticks){
+        
+    }
+}
