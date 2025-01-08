cb9ca2b022c78ebc39b9ea243212022a319076f5
Aashgarg <garg_aashna@icloud.com>
2025-01-08 16:57:43 -0600

    Macro and Intake :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macro.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Macro {
+
+    private static DiffyClaw diffy;
+    private static Intake intake;
+    public Macro(OpMode opmode){
+
+    }
+
+    public static void macro_run(){
+
+    }
+}
