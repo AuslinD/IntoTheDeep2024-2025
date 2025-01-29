5277128a8a90608066d3a99e984e65e40de6207e
Aashgarg <garg_aashna@icloud.com>
2025-01-29 15:55:46 -0600

    macro class update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macro.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Macro {
 
+    static OpMode opMode;
     private static DiffyClaw diffy;
     private static Intake intake;
     public Macro(OpMode opmode){
 
+        Macro.diffy = new DiffyClaw(opMode);
+        Macro.intake = new Intake(opMode);
     }
 
-    public static void macro_run(){
+    public static void macro_run(OpMode opmode){
 
     }
 }
